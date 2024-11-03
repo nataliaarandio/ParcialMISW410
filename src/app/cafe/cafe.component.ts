a9db1239963f8f735708451565ce4fc8cef3ebda
Natalia Arandio <a.arandio@uniandes.edu.co>
2024-11-03 01:43:27 -0500

    Punto 2

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -11,11 +11,34 @@ export class CafeComponent implements OnInit {
 
   constructor(private cafeService: CafeService) { }
   cafes: Array<Cafe> = [];
+  cafeOrigen= 0;
+  cafeBlend = 0;
 
   getCafeList() {
     this.cafeService.getCafeList().subscribe(cafes => {
-      this.cafes = cafes;
+    this.cafes = cafes;
+    this.getTypeCafes(); 
     });
+  }
+  
+  getTypeCafes() {
+    this.cafes.forEach(cafe => {
+      if (cafe.tipo === 'Café de Origen') {
+        this.cafeOrigen++;
+      } else if (cafe.tipo === 'Blend') {
+        this.cafeBlend++;
+      }
+    });
+  }
+
+  getTotalCantOrigen()
+  {
+    return this.cafeOrigen
+  }
+
+  getTotalCantBlend()
+  {
+    return this.cafeBlend
   }
 
   ngOnInit() {
