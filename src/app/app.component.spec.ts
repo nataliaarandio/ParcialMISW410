04c544a70ff5d1f6e8c962c7ab3c0948254ccc7d
Natalia Arandio <a.arandio@uniandes.edu.co>
2024-11-03 03:05:22 -0500

    Punto 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CafeComponent } from './cafe/cafe.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterModule.forRoot([]), HttpClientTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,CafeComponent
       ],
     }).compileComponents();
   });
@@ -20,16 +22,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ParcialMISW410'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ParcialMISW410');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ParcialMISW410');
-  });
 });
--- src/app/cafe/cafe.service.spec.ts
+++ src/app/cafe/cafe.service.spec.ts
@@ -1,16 +1,63 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, waitForAsync, inject, ComponentFixture } from '@angular/core/testing';
 import { CafeService } from './cafe.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CafeComponent } from './cafe.component';
+import { DebugElement } from '@angular/core';
+import { Cafe } from './cafe';
+import { faker } from '@faker-js/faker';
+import { generate, of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CafeModule } from './cafe.module';
 
 describe('Service: Cafe', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CafeService]
-    });
-  });
+  let component: CafeComponent;
+  let fixture: ComponentFixture<CafeComponent>;
+  let compiled: HTMLElement;
+  let cafeServiceSpy: jasmine.SpyObj<CafeService>;
 
-  it('should ...', inject([CafeService], (service: CafeService) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(waitForAsync(() => {
+      const spy = jasmine.createSpyObj('CafeService', ['getCafeList']);
+      TestBed.configureTestingModule({
+        imports: [HttpClientModule],
+        declarations: [ CafeComponent ],
+        providers: [{ provide: CafeService, useValue: spy }]
+      })
+      .compileComponents();
+      cafeServiceSpy = TestBed.inject(CafeService) as jasmine.SpyObj<CafeService>;
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(CafeComponent);
+      component = fixture.componentInstance;
+  
+      let cafes = Array.from({ length: 3 }, () => ({
+        id: faker.string.alphanumeric(15),
+        nombre: faker.lorem.sentences(2),
+        tipo: faker.lorem.sentences(1),
+        region:  faker.location.country(),
+        sabor: faker.lorem.sentence(),
+        altura: faker.number.int({ min: 1800, max: 4000 }),
+        imagen:  faker.image.url(),
+    }));
+
+      cafeServiceSpy.getCafeList.and.returnValue(of(cafes));
+      component.ngOnInit();
+      fixture.detectChanges();
+      compiled = fixture.nativeElement as HTMLElement;
+
+  
+    });
+
+    it('should 4 columns ', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelectorAll('thead th').length).toBe(4);
+    });
+
+    it('should 4 table  tr', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelectorAll('.tr').length).toBe(3);
+    });
 });
